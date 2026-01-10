f0a48ca17a1b1dacb0fc9ce5088623fc6ec850a5
YUS / PIS <yusrilhafis@gmail.com>
2026-01-10 13:46:12 +0700

    feat: get user balance information

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,3 +1,11 @@
+import { pool } from "../../config/db";
+
 export class TransactionService {
-    
+  getBalance = async (email: string) => {
+    const result = await pool.query(
+      `SELECT balance FROM users WHERE email = $1`,
+      [email]
+    );
+    return result.rows[0];
+  };
 }
